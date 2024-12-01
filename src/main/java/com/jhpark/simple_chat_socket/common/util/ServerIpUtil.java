c45c77aaf6759b064bd957d0dd061b06590741f3
박지훈 <qkrwlgns0510@gmail.com>
2024-12-01 21:01:35 +0900

    test : get aws private ip test

--- src/main/java/com/jhpark/simple_chat_socket/common/util/ServerIpUtil.java
+++ src/main/java/com/jhpark/simple_chat_socket/common/util/ServerIpUtil.java
@@ -1,29 +1,43 @@
 package com.jhpark.simple_chat_socket.common.util;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
+import org.springframework.cloud.aws.context.support.env.AwsCloudEnvironmentCheckUtils;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+
+@Component
 public class ServerIpUtil {
 
-     public static String getServerIp() {
-        try {
-            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-            while (interfaces.hasMoreElements()) {
-                NetworkInterface iface = interfaces.nextElement();
-                if (iface.isLoopback() || !iface.isUp()) continue;
+    private final Environment environment;
 
-                Enumeration<InetAddress> addresses = iface.getInetAddresses();
-                while (addresses.hasMoreElements()) {
-                    InetAddress addr = addresses.nextElement();
-                    if (addr.isSiteLocalAddress()) {
-                        return addr.getHostAddress();
-                    }
-                }
-            }
-            throw new RuntimeException("No valid IP address found");
+    public ServerIpUtil(Environment environment) {
+        this.environment = environment;
+    }
+
+    public String getServerIp() {
+        // AWS 환경 감지
+        if (AwsCloudEnvironmentCheckUtils.isRunningOnCloudEnvironment()) {
+            return getAwsPrivateIp();
+        } else {
+            return getLocalIp();
+        }
+    }
+
+    private String getAwsPrivateIp() {
+        String privateIp = environment.getProperty("cloud.aws.instance.private-ip-address");
+        if (privateIp != null) {
+            return privateIp;
+        } else {
+            throw new RuntimeException("Private IP address not found in AWS metadata");
+        }
+    }
+
+    private String getLocalIp() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
         } catch (Exception e) {
-            throw new RuntimeException("Failed to fetch container IP", e);
+            throw new RuntimeException("Failed to retrieve local IP address", e);
         }
     }
 }
